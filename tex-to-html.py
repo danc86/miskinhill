5f38b0f3a7cff703aa4d306d74b274481f4f49f9
Dan Callaghan <djc@djc.id.au>
2008-12-10 17:26:00 +1000

    HTML-ified Wells from issue 21
    --HG--
    extra : convert_revision : c3f91aa50534c7322ba1cb801beae060e86c76fa

--- tex-to-html.py
+++ tex-to-html.py
@@ -7,18 +7,37 @@ def tex2html(s):
     # nbsp
     s = re.sub(r'\~', '&nbsp;', s)
 
-    # non-sentence spaces
+    # ampersands
+    s = re.sub(r'\\&', '&amp;', s)
+
+    # spaces
     s = re.sub(r'\\ ', ' ', s)
+    s = re.sub(r'\\,', '&thinsp;', s)
 
     # emphasis
     s = re.compile(r'\\textit\{([^}]+)\}', re.M).sub(r'<em>\1</em>', s)
 
     # footnotes
+    s += '\n\n<h3>Footnotes</h3>'
     i = 0
     while True:
         i += 1
-        s, n = re.compile(r'\\footnote\{([^}]+)\}', re.M).subn('<a href="#fn-%d" class="footnote-anchor">%d</a> <small id="fn-%d" class="footnote"><span class="footnote-number">%d</span> \\1</small>' % ((i,) * 4), s)
-        if not n: break
+        m = re.compile(r'\\footnote\{([^}]+)\}', re.M).search(s)
+        if not m: break
+        s = s[:m.start()] + \
+            ('<a href="#fn-%d" class="footnote-anchor">%d</a>' % (i, i)) + \
+            s[m.end():] + \
+            ('\n<div id="fn-%d" class="footnote"><p><span class="footnote-number">%d</span> ' % (i, i)) + \
+            m.group(1) + \
+            '</p></div>'
+
+    # sections
+    s = re.sub(r'\\subsection\{([^}]*)\}', r'<h3>\1</h3>', s)
+
+    # containing element
+    s = re.sub(r'\\begin\{article\}\{[^}]*\}\{[^}]*\}\{[^}]*\}', '', s)
+    s = re.sub(r'\\end\{article\}', '', s)
+    s = '<div xmlns="http://www.w3.org/1999/xhtml" class="article-body">' + s + '\n\n</div>'
 
     return s
 
