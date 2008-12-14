29cda4a9ece2056c29e5d14dbb385c3718cf9c02
Dan Callaghan <djc@djc.id.au>
2008-12-14 22:43:50 +1000

    working indexer, more or less
    --HG--
    extra : convert_revision : c3319c0a4ccabc05f4ee9a242bdf28d7c2444cfc

--- search/indexer/src/main/java/au/com/miskinhill/search/Article.java
+++ search/indexer/src/main/java/au/com/miskinhill/search/Article.java
@@ -1,16 +1,26 @@
 package au.com.miskinhill.search;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.SequenceInputStream;
+
+import javax.xml.stream.XMLStreamException;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
 
 import au.com.miskinhill.search.analysis.RDFLiteralTokenizer;
+import au.com.miskinhill.search.analysis.XMLTokenizer;
 import au.com.miskinhill.search.analysis.RDFLiteralTokenizer.UnknownLiteralTypeException;
 
+import com.hp.hpl.jena.rdf.model.AnonId;
 import com.hp.hpl.jena.rdf.model.Literal;
-import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.RDFVisitor;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
@@ -19,16 +29,46 @@ public class Article implements Indexable {
 	
 	private Document doc = new Document();
 	
-	public Article(Resource res) throws UnknownLiteralTypeException {
+	private static final byte[] XHTML_STRICT_DTD_DECL = 
+			("<!DOCTYPE html " +
+			"PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" " +
+			"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n")
+			.getBytes();
+	
+	public Article(Resource res) throws UnknownLiteralTypeException, FileNotFoundException, XMLStreamException {
 		StmtIterator i = res.listProperties();
 		while (i.hasNext()) {
-			Statement stmt = i.nextStatement();
-			RDFNode n = stmt.getObject();
-			if (n.isLiteral()) {
-				doc.add(new Field(stmt.getPredicate().getURI(), 
-						RDFLiteralTokenizer.fromLiteral((Literal) n.as(Literal.class))));
-			}
+			final Statement stmt = i.nextStatement();
+			stmt.getObject().visitWith(new RDFVisitor() {
+				public Object visitBlank(Resource r, AnonId id) {
+					/* pass */
+					return null;
+				}
+
+				public Object visitLiteral(Literal literal) {
+					try {
+						doc.add(new Field(stmt.getPredicate().getURI(), 
+								RDFLiteralTokenizer.fromLiteral(literal)));
+					} catch (UnknownLiteralTypeException e) {
+						throw new RuntimeException(e);
+					}
+					return null;
+				}
+
+				@Override
+				public Object visitURI(Resource r, String uri) {
+					return null;
+				}
+				
+			});
 		}
+		
+		assert res.getURI().substring(0, 25) == "http://miskinhill.com.au/";
+		File content = new File("../../content/" + res.getURI().substring(25) + ".html");
+		doc.add(new Field("content", new XMLTokenizer(
+				new SequenceInputStream(
+					new ByteArrayInputStream(XHTML_STRICT_DTD_DECL), 
+					new BufferedInputStream(new FileInputStream(content))))));
 	}
 	
 	public void addToIndex(IndexWriter iw) throws IOException {
