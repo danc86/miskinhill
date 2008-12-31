cf68aeb46aa45571da9b89601b650ab9ba0a29ee
Dan Callaghan <djc@djc.id.au>
2008-12-31 20:02:44 +1000

    use our custom query parser and analyzer
    --HG--
    extra : convert_revision : 3d27f4c9204685e6e8492a999dc9cc22fa532f36

--- search/search-webapp/src/main/java/au/com/miskinhill/search/webapp/SearchServlet.java
+++ search/search-webapp/src/main/java/au/com/miskinhill/search/webapp/SearchServlet.java
@@ -7,48 +7,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.queryParser.ParseException;
-import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
+
+import au.com.miskinhill.search.analysis.MHAnalyzer;
 
 public class SearchServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1140261890520211317L;
 	
 	private static IndexReader index;
-	private static QueryParser parser;
 	static {
 		try {
 			index = IndexReader.open(FSDirectory.getDirectory("../index-data"), true);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-		
-		parser = new QueryParser("content", new StandardAnalyzer());
 	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		String query = req.getParameter("q");
-		if (query == null) {
+		String q = req.getParameter("q");
+		if (q == null) {
 			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing q argument");
 			return;
 		}
 		
 		IndexSearcher searcher = new IndexSearcher(index);
-		TopDocs results;
-		try {
-			results = searcher.search(parser.parse(query), 50);
-		} catch (ParseException e) {
-			resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
-					"Unable to parse query");
-			return;
-		}
+		Query query = MultilingualQueryParser.parse(q, new MHAnalyzer());
+		TopDocs results = searcher.search(query, 50);
 		
 		resp.setContentType("text/plain");
 		for (int i = 0; i < results.totalHits; i++) {
