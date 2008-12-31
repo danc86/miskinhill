5798df964971c08d79144a8587625ebbfe34cf9e
Dan Callaghan <djc@djc.id.au>
2008-12-31 22:05:48 +1000

    render search results in a velocity template
    --HG--
    extra : convert_revision : 16af2df2ef63ba445b6bc32284f04776084a4f6c

--- search/search-webapp/src/main/java/au/com/miskinhill/search/webapp/SearchServlet.java
+++ search/search-webapp/src/main/java/au/com/miskinhill/search/webapp/SearchServlet.java
@@ -1,17 +1,25 @@
 package au.com.miskinhill.search.webapp;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 
 import au.com.miskinhill.search.analysis.MHAnalyzer;
 
@@ -31,22 +39,56 @@ public class SearchServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		String q = req.getParameter("q");
-		if (q == null) {
-			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing q argument");
-			return;
+		try {
+			String q = req.getParameter("q");
+			if (q == null) {
+				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing q argument");
+				return;
+			}
+			
+			IndexSearcher searcher = new IndexSearcher(index);
+			Query query = MultilingualQueryParser.parse(q, new MHAnalyzer());
+			TopDocs topDocs = searcher.search(query, 50);
+			List<Result> results = new ArrayList<Result>();
+			for (int i = 0; i < topDocs.totalHits; i ++) {
+				Document doc = index.document(topDocs.scoreDocs[i].doc);
+				Result result = new Result(doc.get("url"), topDocs.scoreDocs[i].score);
+				results.add(result);
+			}
+			
+			VelocityContext context = new VelocityContext();
+			context.put("q", q);
+			context.put("results", results);
+			Template template = initVelocity().getTemplate(
+					"/au/com/miskinhill/search/webapp/SearchResultsTemplate.vm");
+			resp.setContentType("text/html; charset=utf-8");
+			template.merge(context, resp.getWriter());
+		} catch (Exception e) {
+			// Java is lame
+			throw new ServletException(e);
 		}
-		
-		IndexSearcher searcher = new IndexSearcher(index);
-		Query query = MultilingualQueryParser.parse(q, new MHAnalyzer());
-		TopDocs results = searcher.search(query, 50);
-		
-		resp.setContentType("text/plain");
-		for (int i = 0; i < results.totalHits; i++) {
-			resp.getOutputStream().write(
-					(results.scoreDocs[i].doc + ": "
-							+ results.scoreDocs[i].score + "\n").getBytes());
+	}
+	
+	// XXX lame
+	private VelocityEngine initVelocity() throws Exception {
+		Properties props = new Properties();
+		props.load(SearchServlet.class.getResourceAsStream("/velocity.properties"));
+		VelocityEngine ve = new VelocityEngine();
+		ve.setApplicationAttribute(ServletContext.class.getName(), 
+				getServletContext());
+		ve.init(props);
+		return ve;
+	}
+	
+	public static class Result {
+		private String url;
+		private float score;
+		public Result(String url, float score) {
+			this.url = url;
+			this.score = score;
 		}
+		public String getUrl() { return url; }
+		public float getScore() { return score; }
 	}
 
 }
