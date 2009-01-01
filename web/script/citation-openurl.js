ca13c0b671505c817547956026b00c18bca4475d
Dan Callaghan <djc@djc.id.au>
2009-01-02 02:21:45 +1000

    breadcrumbs, more style fiddling, form to activate openurl
    --HG--
    extra : convert_revision : 2c21c2aaaf68fb44453ccd2c79753525be2ddeb9

--- web/script/citation-openurl.js
+++ web/script/citation-openurl.js
@@ -1,19 +1,16 @@
-var baseurl = 'http://getcopy.edina.ac.uk/resolve/';
+var default_baseurl = 'http://getcopy.edina.ac.uk/resolve/';
 
-var book_referent_fields = ['btitle', 'isbn', 'au', 'place', 'pub', 'date', 'spage', 'epage'];
-var bookitem_referent_fields = ['btitle', 'atitle', 'isbn', 'au', 'place', 'pub', 'date', 'spage', 'epage'];
-var article_referent_fields = ['atitle', 'jtitle', 'issn', 'au', 'volume', 'issue', 'date', 'spage', 'epage'];
-var openurl_qs_from_citation = function (citation) {
+var fields = [
+    'atitle', 'btitle', 'jtitle', 'isbn', 'issn', 'au', 'volume', 
+    'issue', 'date', 'place', 'pub', 'date', 'spage', 'epage'
+];
+var openurl_qs_from_citation = function (baseurl, citation) {
     var openurl_args = ['url_ver=Z39.88-2004'];
-    var fields = undefined;
     if ($(citation).hasClass('book')) {
-        fields = book_referent_fields;
         openurl_args.push('rft_val_fmt=info:ofi/fmt:kev:mtx:book', 'rft.genre=book');
     } else if ($(citation).hasClass('bookitem')) {
-        fields = bookitem_referent_fields;
         openurl_args.push('rft_val_fmt=info:ofi/fmt:kev:mtx:book', 'rft.genre=bookitem');
     } else if ($(citation).hasClass('article')) {
-        fields = article_referent_fields;
         openurl_args.push('rft_val_fmt=info:ofi/fmt:kev:mtx:journal', 'rft.genre=article');
     }
     $.each(fields, function () {
@@ -23,12 +20,23 @@ var openurl_qs_from_citation = function (citation) {
             openurl_args.push('rft.' + encodeURIComponent(field) + '=' + encodeURIComponent(value));
         });
     });
-    $(citation).append(' <a class="openurl" href="' + baseurl + '?' + openurl_args.join('&') + '"><img src="/images/silk/world_link.png" alt="OpenURL" /></a>');
+    $(citation).append(' <a class="openurl" href="' + baseurl + '?' + openurl_args.join('&') + '">' +
+            '<img src="/images/silk/world_link.png" alt="OpenURL" /></a>');
     $(citation).find('a.openurl').hover(
             function () { $(citation).addClass('openurl-hover'); }, 
             function () { $(citation).removeClass('openurl-hover'); });
 };
 
 $(document).ready(function () {
-    $('.citation').each(function () { openurl_qs_from_citation(this); });
+    $('.metabox-container:first').append(
+            '<div class="openurl-control metabox"><p>' + 
+            '   <label for="openurl-baseurl">OpenURL resolver:</label>' +
+            '   <input id="openurl-baseurl" type="text" value="' + default_baseurl + '" />' +
+            '   <button id="show-openurl">Show OpenURL links</button>' +
+            '</p></div>');
+    $('#show-openurl').click(function () {
+        $('.citation').each(function () {
+            openurl_qs_from_citation($('#openurl-baseurl').attr('value'), this);
+        });
+    });
 });
