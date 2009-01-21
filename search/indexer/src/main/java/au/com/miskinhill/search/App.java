048bf79ce6ba005b55eff868aa477f0ea87dcff2
Dan Callaghan <djc@djc.id.au>
2009-01-21 10:03:33 +1000

    always cleanly close the index

--- search/indexer/src/main/java/au/com/miskinhill/search/App.java
+++ search/indexer/src/main/java/au/com/miskinhill/search/App.java
@@ -25,33 +25,37 @@ public class App {
 
     private static void writeIndex(final String contentPath, final String indexPath) 
             throws Exception {
-		Model schemaModel = ModelFactory.createDefaultModel();
-		schemaModel.read(new FileInputStream(new File(contentPath + "/rdfschema/schema.ttl")), "", "TURTLE");
+        
+        Model schemaModel = ModelFactory.createDefaultModel();
+        schemaModel.read(new FileInputStream(new File(contentPath + "/rdfschema/schema.ttl")), "", "TURTLE");
+        
+        Model metadataModel = ModelFactory.createDefaultModel();
+        metadataModel.read(new FileInputStream(new File(contentPath + "/meta.nt")), "", "N-TRIPLE");
+        
+        InfModel model = ModelFactory.createRDFSModel(ModelFactory.createUnion(schemaModel, metadataModel));
+        
+        IndexWriter iw = new IndexWriter(FSDirectory.getDirectory(indexPath), 
+                NullAnalyzer.INSTANCE, 
+                /* create */ true, 
+                MaxFieldLength.UNLIMITED);
+        try {
+    		iw.setUseCompoundFile(false);
+    		
+    		FulltextFetcher fulltextFetcher = new FulltextFetcher(contentPath);
+    		
+    		ResIterator articles = model.listSubjects();
+    		while (articles.hasNext()) {
+    			GenericResource r = GenericResource.fromRDF(articles.nextResource(), fulltextFetcher);
+    			if (r != null) {
+    			    r.addToIndex(iw);
+    			}
+    		}
 		
-		Model metadataModel = ModelFactory.createDefaultModel();
-		metadataModel.read(new FileInputStream(new File(contentPath + "/meta.nt")), "", "N-TRIPLE");
-		
-		InfModel model = ModelFactory.createRDFSModel(ModelFactory.createUnion(schemaModel, metadataModel));
-		
-		IndexWriter iw = new IndexWriter(FSDirectory.getDirectory(indexPath), 
-				NullAnalyzer.INSTANCE, 
-				/* create */ true, 
-				MaxFieldLength.UNLIMITED);
-		iw.setUseCompoundFile(false);
-		
-		FulltextFetcher fulltextFetcher = new FulltextFetcher(contentPath);
-		
-		ResIterator articles = model.listSubjects();
-		while (articles.hasNext()) {
-			GenericResource r = GenericResource.fromRDF(articles.nextResource(), fulltextFetcher);
-			if (r != null) {
-			    r.addToIndex(iw);
-			}
-		}
-		
-		iw.commit();
-		iw.optimize();
-		iw.close();
+    		iw.commit();
+    		iw.optimize();
+        } finally {
+            iw.close();
+        }
     }
 	
 	public static void main(String[] args) throws Exception {
