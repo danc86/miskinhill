2146db764783d55e3f5e134c4d4e4b9d9f53aad6
Dan Callaghan <djc@djc.id.au>
2009-03-09 18:10:39 +1000

    fiddling with floating footnotes

--- web/script/footnote.js
+++ web/script/footnote.js
@@ -1,26 +1,46 @@
-var show_footnote = function () {
-    $('.floating-footnote').remove();
-    var id = this.hash;
-    var ff = $(id).clone(true).addClass('floating-footnote').hide();
-    ff.css('left', $(this).parent().offset().left + 'px');
-    ff.css('top', ($(this).offset().top + 20) + 'px');
-    ff.prepend('<div style="float: right;">' +
+// this should be in jquery probably
+var bind = function (func, subj) {
+    return function () { return func.call(subj); };
+};
+
+function FloatingFootnote(footnote_anchor) {
+    this.footnote_selector = footnote_anchor.hash;
+    this.div = $(this.footnote_selector).clone(true);
+    this.div.hide();
+    this.div.addClass('floating-footnote');
+    this.div.css('left', $('.body-text').offset().left + 'px');
+    this.div.css('top', ($(footnote_anchor).offset().top + 10) + 'px');
+    this.div.prepend('<div style="float: right;">' +
             '<a href="#" class="close-button"><img src="/images/silk/cross.png" alt="[close]" title="Close this footnote" /></a>' + 
             '</div>');
-    ff.find('.close-button').click(function () {
-        ff.fadeOut('normal', function () { ff.remove(); });
-        return false;
-    });
-    ff.insertAfter($(id)); // doesn't really matter where we insert it, but want to avoid stuffing up p + p styles
-    ff.slideDown();
-    return false;
+    this.div.find('.close-button').click(bind(this.close, this));
+    this.div.insertAfter($(this.footnote_selector)); // doesn't really matter where we insert it, but want to avoid stuffing up p + p styles
+    FloatingFootnote.instances[this.footnote_selector] = this;
+}
+FloatingFootnote.prototype.open = function () {
+    this.div.slideDown('normal');
+    // close all others
+    for (s in FloatingFootnote.instances) {
+        if (s != this.footnote_selector)
+            FloatingFootnote.instances[s].close();
+    }
 };
-
-var hide_footnote = function () {
-    $('.floating-footnote').slideUp('fast', function () { $(this).remove(); });
-    return false;
+FloatingFootnote.prototype.close = function () {
+    delete FloatingFootnote.instances[this.footnote_selector];
+    var div = this.div;
+    this.div.fadeOut('normal', function () { div.remove(); });
+    return false; // for onclick
 };
+FloatingFootnote.instances = {}; // map of footnote id selector to FloatingFootnote instance
 
 $(document).ready(function () {
-    $('.footnote-anchor').click(show_footnote);
+    $('.footnote-anchor').click(function () {
+        if (FloatingFootnote.instances[this.hash]) {
+            // one is already shown, close it
+            FloatingFootnote.instances[this.hash].close();
+        } else {
+            new FloatingFootnote(this).open();
+        }
+        return false;
+    });
 });
