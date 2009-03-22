d033ce5a8e369a1ad27407bc265a1a4f5c169ff1
Dan Callaghan <djc@djc.id.au>
2009-03-22 12:02:38 +1000

    show ISSN on journal page

--- web/test_templates.py
+++ web/test_templates.py
@@ -5,9 +5,15 @@ import os, sys
 import unittest
 import tempfile
 from genshi.template import NewTextTemplate
+import lxml.etree, lxml.html
 
 import rdfob
 from app import template_loader
+
+class MockRequest(object):
+
+    def __init__(self):
+        self.script_name = ''
 
 class EndnoteArticleTemplateTest(unittest.TestCase):
 
@@ -20,6 +26,19 @@ class EndnoteArticleTemplateTest(unittest.TestCase):
     def test_markup_stripped(self):
         title, = [line for line in self.result.splitlines() if line.startswith('%T')]
         self.assertEquals(u'%T Moscow 1937: the interpreter’s story', title)
+
+class HtmlJournalTemplateTest(unittest.TestCase):
+
+    def setUp(self):
+        graph = rdfob.Graph(os.path.join(os.path.dirname(__file__), 'testdata', 'templates', 'meta.nt'))
+        node = graph[rdfob.URIRef(u'http://miskinhill.com.au/journals/test/')]
+        result = template_loader.load(os.path.join('html', 'journal.xml'))\
+                .generate(req=MockRequest(), node=node).render('xhtml', encoding=None)
+        self.root = lxml.html.fromstring(result, parser=lxml.html.XHTMLParser())
+
+    def test_issn_appears(self):
+        issn, = self.root.find_class('issn')
+        self.assertEquals('ISSN 12345678', issn.text.strip())
 
 if __name__ == '__main__':
     unittest.main()
