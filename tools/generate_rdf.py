bb187b0ae52b343434f6f3f4a56f9d1a994b5c61
Dan Callaghan <djc@djc.id.au>
2009-08-02 12:17:56 +1000

    extract roles from mhs:responsibility XML

--- tools/generate_rdf.py
+++ tools/generate_rdf.py
@@ -5,12 +5,15 @@ import os
 import subprocess
 from cStringIO import StringIO
 import RDF
+import lxml.etree, lxml.html
+from itertools import chain
 
 RDF_NS = RDF.NS('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
 RDFS_NS = RDF.NS('http://www.w3.org/2000/01/rdf-schema#')
 OWL_NS = RDF.NS('http://www.w3.org/2002/07/owl#')
 DCTERMS_NS = RDF.NS('http://purl.org/dc/terms/')
 PRISM_NS = RDF.NS('http://prismstandard.org/namespaces/1.2/basic/')
+MHS_NS = RDF.NS('http://miskinhill.com.au/rdfschema/1.0/')
 
 def load():
     g = RDF.Model()
@@ -21,6 +24,24 @@ def load():
                 g.load('file:' + os.path.join(dirpath, filename))
                 print >>sys.stderr, '%d triples' % len(g)
     return g
+
+def extract(g):
+    print >>sys.stderr, 'Extracting ...',
+    for stmt in chain(g.find_statements(RDF.Statement(subject=None, predicate=MHS_NS.responsibility, object=None)),
+            g.find_statements(RDF.Statement(subject=None, predicate=DCTERMS_NS.title, object=None))):
+        responsibility = lxml.html.fromstring(stmt.object.literal_value['string'])
+        for anchor in responsibility.findall('a'):
+            href = RDF.Uri(anchor.get('href'))
+            if MHS_NS.Author in _types(g, href):
+                rels = anchor.get('rel', '').split()
+                if 'contributor' in rels:
+                    predicate = DCTERMS_NS.contributor
+                elif 'translator' in rels:
+                    predicate = MHS_NS.translator
+                else:
+                    predicate = DCTERMS_NS.creator
+                g.append(RDF.Statement(stmt.subject, predicate=predicate, object=href))
+    print >>sys.stderr, '%d triples' % len(g)
 
 def _types(g, s):
     return g.get_targets(source=s, predicate=RDF_NS.type)
@@ -35,7 +56,7 @@ def transitive_objects(g, subject, predicate):
     return retval
 
 def infer(g):
-    print >>sys.stderr, 'Inferring superclasses/superproperties ...'
+    print >>sys.stderr, 'Inferring superclasses/superproperties ...',
     for s in frozenset(stmt.subject for stmt in g):
         for t in list(_types(g, s)):
             for ct in transitive_objects(g, t, RDFS_NS.subClassOf):
@@ -43,6 +64,7 @@ def infer(g):
     for stmt in g:
         for cp in transitive_objects(g, stmt.predicate, RDFS_NS.subPropertyOf):
             g.append(RDF.Statement(stmt.subject, cp, stmt.object))
+    print >>sys.stderr, '%d triples' % len(g)
 
 RANGE_PROPERTY_EXCEPTIONS = frozenset([DCTERMS_NS.publisher, DCTERMS_NS.identifier, DCTERMS_NS.coverage, PRISM_NS.publicationDate])
 RANGE_OBJECT_EXCEPTIONS = frozenset([RDF.Node(uri_string='http://www.w3.org/TR/2000/CR-rdf-schema-20000327#Literal')])
@@ -74,6 +96,7 @@ def validate(g):
 
 def main():
     g = load()
+    extract(g)
     infer(g)
     validate(g)
     sys.stdout.write(g.to_string())
