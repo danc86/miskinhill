9745960caef22d04707ad46fe93fa6302b5fad3b
Dan Callaghan <djc@djc.id.au>
2009-08-16 21:51:20 +1000

    fade out notifications after 20 seconds

--- web/script/notification.js
+++ web/script/notification.js
@@ -3,5 +3,6 @@ $(document).ready(function () {
     if (notifs) {
         var notif = unescape(notifs[1]);
         $('#notification-placeholder').append('<div class="notification">' + notif + '</div>');
+        $('.notification').oneTime(20000, function () { $(this).fadeOut('normal'); });
     }
 });
