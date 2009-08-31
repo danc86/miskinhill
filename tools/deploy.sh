1ec08f88b7031f21546fb36a8b16d3f4457eb31b
Dan Callaghan <djc@djc.id.au>
2009-08-31 16:34:23 +1000

    don't need .hg on syn

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avz --delete-during --delete-excluded --exclude work --exclude final --exclude '.*.sw?' . syn:~/.www/miskinhill.com.au/content/
+rsync -avz --delete-during --delete-excluded --exclude work --exclude final --exclude .hg --exclude '.*.sw?' . syn:~/.www/miskinhill.com.au/content/
