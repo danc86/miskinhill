d74ad301ceff5f3d49fa831c870d07c854bd9110
Dan Callaghan <djc@djc.id.au>
2009-10-11 16:40:10 +1000

    unused import

--- rdftemplate/src/main/java/au/com/miskinhill/rdftemplate/selector/InvalidSelectorSyntaxException.java
+++ rdftemplate/src/main/java/au/com/miskinhill/rdftemplate/selector/InvalidSelectorSyntaxException.java
@@ -1,7 +1,5 @@
 package au.com.miskinhill.rdftemplate.selector;
 
-import org.antlr.runtime.RecognitionException;
-
 public class InvalidSelectorSyntaxException extends RuntimeException {
     
     private static final long serialVersionUID = 5805546105865617336L;
