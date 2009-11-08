c4d9fa6f1572e7f342678c9484d92c4b74cc401e
Dan Callaghan <djc@djc.id.au>
2009-11-08 13:43:11 +1000

    hardcoded resolver to always fail if not resolved locally; added StAX XMLResolver support, including a dodgy hack because stupid StAX doesn't resolve relative URLs apparently

--- html-dtd-cache/src/main/java/au/com/miskinhill/xhtmldtd/XhtmlEntityResolver.java
+++ html-dtd-cache/src/main/java/au/com/miskinhill/xhtmldtd/XhtmlEntityResolver.java
@@ -1,23 +1,18 @@
 package au.com.miskinhill.xhtmldtd;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+
+import javax.xml.stream.XMLResolver;
+import javax.xml.stream.XMLStreamException;
 
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-public class XhtmlEntityResolver implements EntityResolver {
+public class XhtmlEntityResolver implements EntityResolver, XMLResolver {
     static final String XHTML_PREFIX="http://www.w3.org/TR/xhtml1/DTD/";
-    final EntityResolver next;
-    
-    public XhtmlEntityResolver(EntityResolver next) {
-        this.next = next;
-    }
-
-    public XhtmlEntityResolver() {
-        this(null);
-    }
 
     @Override
     public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
@@ -37,9 +32,34 @@ public class XhtmlEntityResolver implements EntityResolver {
         if(systemId.startsWith("file:") || systemId.startsWith("jar:")) {
         	return null;
         }
-        if(next != null)
-            return next.resolveEntity(publicId, systemId);
-        else
+        throw new IllegalStateException("Entity should have been resolved from the cache");
+    }
+
+    @Override
+    public Object resolveEntity(String publicId, String systemId, String baseURI, String namespace)
+            throws XMLStreamException {
+        if (systemId.startsWith(XHTML_PREFIX)) {
+            String name = systemId.substring(XHTML_PREFIX.length());
+            InputStream resourceAsStream = getClass().getResourceAsStream(name);
+            if (resourceAsStream != null) {
+                return resourceAsStream;
+            }
+        }
+        
+        // Let file: URLs just get loaded using the default mechanism
+        if (systemId.startsWith("file:") || systemId.startsWith("jar:")) {
             return null;
+        }
+        
+        // stupid fucking hack because I don't have a real catalog resolver or whatever, blerg
+        if (systemId.startsWith("xhtml")) {
+            // means it's just an unresolved relative URL that is referenced from a DTD
+            InputStream resourceAsStream = getClass().getResourceAsStream(systemId);
+            if (resourceAsStream != null) {
+                return resourceAsStream;
+            }
+        }
+        
+        throw new IllegalStateException("Entity should have been resolved from the cache");
     }
 }
--- html-dtd-cache/src/test/java/au/com/miskinhill/xhtmldtd/TestXhtmlEntityResolver.java
+++ html-dtd-cache/src/test/java/au/com/miskinhill/xhtmldtd/TestXhtmlEntityResolver.java
@@ -1,16 +1,12 @@
 package au.com.miskinhill.xhtmldtd;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-import au.com.miskinhill.xhtmldtd.FailingEntityResolver;
-import au.com.miskinhill.xhtmldtd.XhtmlEntityResolver;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 
 public class TestXhtmlEntityResolver {
 
@@ -26,21 +22,21 @@ public class TestXhtmlEntityResolver {
     @Test
     public void testResolver() throws Exception {
         DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        documentBuilder.setEntityResolver(new XhtmlEntityResolver(new FailingEntityResolver()));
+        documentBuilder.setEntityResolver(new XhtmlEntityResolver());
         documentBuilder.parse(getClass().getResource("sample.xhtml").toString());
     }
     
     @Test
     public void testResolverStrict() throws Exception {
         DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        documentBuilder.setEntityResolver(new XhtmlEntityResolver(new FailingEntityResolver()));
+        documentBuilder.setEntityResolver(new XhtmlEntityResolver());
         documentBuilder.parse(getClass().getResource("sample-strict.xhtml").toString());
     }
 
     @Test
     public void testResolverMissingFile() throws Exception {
         DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        documentBuilder.setEntityResolver(new XhtmlEntityResolver(new FailingEntityResolver()));
+        documentBuilder.setEntityResolver(new XhtmlEntityResolver());
         try {
             documentBuilder.parse(getClass().getResource("sample-missing.xhtml").toString());
             Assert.fail("Should have failed to find the DTD!");
