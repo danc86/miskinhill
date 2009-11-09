cc311a884ddeae6173ab90a9e2773d223f2b1c9d
Dan Callaghan <djc@djc.id.au>
2009-11-09 21:43:10 +1000

    deploy to vz

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 rsync -avz --delete-during --delete-excluded --exclude work --exclude final --exclude .hg --exclude '.*.sw?' . syn:~/.www/miskinhill.com.au/content/
+rsync -avz --delete-during --delete-excluded --exclude work --exclude final --exclude .hg --exclude '.*.sw?' . miskinhill:/var/www/miskinhill.com.au/content/
