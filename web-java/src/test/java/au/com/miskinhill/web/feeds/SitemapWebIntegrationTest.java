8b8a626a9779eb90a974fa7ba8d67569c652884c
Dan Callaghan <djc@djc.id.au>
2009-11-20 20:13:32 +1000

    assert uniqueness of sitemap urls

--- web-java/src/test/java/au/com/miskinhill/web/feeds/SitemapWebIntegrationTest.java
+++ web-java/src/test/java/au/com/miskinhill/web/feeds/SitemapWebIntegrationTest.java
@@ -1,11 +1,16 @@
 package au.com.miskinhill.web.feeds;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import com.sun.jersey.api.client.Client;
 import org.junit.Test;
 
 import au.com.miskinhill.AbstractWebIntegrationTest;
+import au.com.miskinhill.schema.sitemaps.Url;
 import au.com.miskinhill.schema.sitemaps.Urlset;
 
 public class SitemapWebIntegrationTest extends AbstractWebIntegrationTest {
@@ -14,6 +19,10 @@ public class SitemapWebIntegrationTest extends AbstractWebIntegrationTest {
     public void shouldBeParseable() {
         Urlset sitemap = Client.create().resource(BASE).path("/feeds/sitemap").get(Urlset.class);
         assertTrue(sitemap.getUrls().size() >= 1228);
+        Set<String> uniqueLocs = new HashSet<String>();
+        for (Url url: sitemap.getUrls())
+            uniqueLocs.add(url.getLoc());
+        assertThat(uniqueLocs.size(), equalTo(sitemap.getUrls().size()));
     }
 
 }
