eb92acb970caf7474a0b4c834e5322b140e8c8d9
Dan Callaghan <djc@djc.id.au>
2009-12-26 19:58:32 +1000

    python version has been completely ported to java now

--- web/script/notification.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    var notifs = new String(document.cookie).match(/miskinhill-notification=([^;]+)/);
-    if (notifs) {
-        var notif = unescape(notifs[1]);
-        $('#notification-placeholder').append('<div class="notification">' + notif + '</div>');
-        $('.notification').oneTime(20000, function () { $(this).fadeOut('normal'); });
-    }
-});
--- web/test_viewutils.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# vim:fileencoding=utf-8
-
-import unittest
-
-import viewutils
-
-if __name__ == '__main__':
-    unittest.main()
