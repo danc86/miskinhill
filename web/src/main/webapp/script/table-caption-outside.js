d2fbf6bb95bdd867c691ba2fd07e82d5c3eece18
Dan Callaghan <djc@djc.id.au>
2011-03-13 17:08:56 +1000

    improve the appearance of data tables (for Lagerberg in vol 24)

---
+++ web/src/main/webapp/script/table-caption-outside.js
@@ -0,0 +1,16 @@
+/* Add support for table-caption: top-outside; if it's not already supported */
+
+var caption_outside_supported = function () {
+	var test_div = document.createElement('div');
+	test_div.style.cssText = 'caption-side: top-outside;';
+	return !!test_div.style.captionSide;
+};
+
+if (!caption_outside_supported()) {
+	$(function () {
+		$('table.data caption').each(function (i, caption) {
+			$('<div class="hacked-data-caption" />').append($(caption).contents()).insertBefore(caption.parentNode);
+			$(caption).remove();
+		});
+	});
+}
