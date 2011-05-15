cf400baa5285d7db5da13fd9789e4daf37ca0897
Dan Callaghan <djc@djc.id.au>
2011-05-15 14:02:58 +1000

    moved content deployment script to content repo

--- tools/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rsync -avz --delete-during --delete-excluded --exclude work --exclude final --exclude .hg --exclude '.*.sw?' . syn:~/.www/miskinhill.com.au/content/
-rsync -avz --delete-during --delete-excluded --exclude work --exclude final --exclude .hg --exclude '.*.sw?' . miskinhill:/var/www/miskinhill.com.au/content/
